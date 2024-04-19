d5790659cd2e4f33b06b25aa348b9e83ba69ffac
Nadav Golan-Yanay <78790309+nadav-golan-yanay@users.noreply.github.com>
2024-04-19 12:09:31 +0300

    Update Extender.cpp
    adding DC motors

--- SRC/Extender.cpp
+++ SRC/Extender.cpp
@@ -8,11 +8,37 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
+/*int IN1;
+int IN2;
+int EN;*/
+
+Extender::Extender(){
+}
+
+Extender::Extender(int dirA, int dirB, int speed){
+  in1 = dirA;
+  in2 = dirB;
+  en = speed;
+
+  /*Serial.println(IN1);
+  Serial.println(IN2);
+  Serial.println(EN);
+
+  delay(2000);*/
+}
+
 void Extender::begin() {
   // put your setup code here, to run once:
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  Serial.print("in1 = ");  
+  Serial.println(in1);
+  Serial.print("in2 = ");  
+  Serial.println(in2);
+  Serial.print("en = ");  
+  Serial.println(en);
 }
 
 void Extender::ServoWrite(int num, int value) {
@@ -25,7 +51,28 @@ void Extender::ServoWrite(int num, int value) {
   pwm.writeMicroseconds(num, value);
 }
 
-void Extender::MotorWrite(int num, int value) {
+void Extender::L925nWrite(int dir, int value) { //DC motor via L982n motor Driver
+  Serial.println(in1);
+  Serial.println(in2);
+  Serial.println(en);
+  if (dir > 0){
+    pwm.setPWM(in1, 0, 4095);
+    pwm.setPWM(in2, 0, 0);
+  } else if (dir < 0){
+    pwm.setPWM(in1, 0, 0);
+    pwm.setPWM(in2, 0, 4095);
+  } else if (dir == 0){
+    pwm.setPWM(in1, 0, 0);
+    pwm.setPWM(in2, 0, 0);
+  } else if (dir == 0 && value == 255){
+    pwm.setPWM(in1, 0, 4095);
+    pwm.setPWM(in2, 0, 4095);
+  }
+  value = map(value, 0, 255, 0, 4095);
+  pwm.setPWM(en, 0, value);
+}
+
+void Extender::ESCWrite(int num, int value) { //Drone motors
   if(value < SERVOMIN)
   {  // treat values less than 544 as angles in degrees (valid values in microseconds are handled as microseconds)
     if(value < 0) value = 0;
