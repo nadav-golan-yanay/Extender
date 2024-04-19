d28a79ff6520c0375e199f4c2ea163661a6ba417
Nadav Golan-Yanay <78790309+nadav-golan-yanay@users.noreply.github.com>
2024-04-19 12:10:05 +0300

    Update Extender.h
    adding DC motors

--- SRC/Extender.h
+++ SRC/Extender.h
@@ -1,11 +1,19 @@
 #ifndef EXTENDER_H
 #define EXTENDER_H
- class Extender {
 
+ class Extender {
   public:
+   Extender(int _in1, int _in2, int _en);
+   Extender();
    void begin();   //must be called from  void setup()
    void ServoWrite(int num, int value);
-   void MotorWrite(int num, int value);
+   void ESCWrite(int num, int value);
+   void L925nWrite(int dir, int value);
+
+  private:
+   int in1 = 0; 
+   int in2 = 0;
+   int en = 0;
 
  };
 #endif
